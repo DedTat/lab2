7f69db9a7ee02b25e74833c7327f04891f6e8040
DedTat <61464184+DedTat@users.noreply.github.com>
2022-07-11 13:41:27 +0300

    Initial commit

---
+++ с циклом/lab2_2018.m
@@ -0,0 +1,106 @@
+% Разложение функции t в ряд Фурье
+%в дискретизированном виде на интервале  [-T,T], например,[-pi,pi]  
+
+clc;
+
+N=4;
+K=2;
+Npow=2;
+while Npow <= 10
+    N=2^Npow;
+    Kpow=1;
+    while Kpow <= 9
+        K=2^Kpow;
+        %N=128; %Количество отсчетов (элементов массива y(t))
+        %K=256; %Количество членов ряда Фурье
+        T=pi; %диапазон изменения функции f(i) равен +/-T
+        kp=4.5 ; %количество периодов гармонической функции
+        y=zeros(1,N+1); 
+        Sa = zeros(1,K);
+        Sb = zeros(1,K);
+        p=2;% показатель степени функции t^p
+        f=zeros(1,N+1);
+        Sa0=0;
+        for i=1:N+1     
+           %f(i)=sin(2*pi*kp*(i-1)/N); % гармоническая функция
+           x(i)= 2*T*(((i-1-N/2))/N);%для интервала от -Т до Т
+           f(i)=i^2;
+           %x(i)=(2*T*(((i-1-N/2))/N));
+          % f(i)=(x(i))^p;
+          % f(i)= (2*T*(((i-1-N/2))/N))^p; %функция t^p
+          % x(i)=(2*T*(((i-1-N/2))/N));
+          % f(i)=abs(x(i));
+            Sa0=Sa0+f(i);
+        end
+        Sa0=Sa0/N; %вычисленный коэф. a0/2
+        %Saa0=pi^2/3 %%теоретически определенные коэф. а0/2 для функции t^2
+        for i=1:N+1
+            for j=1:K
+                Sa(j) = (Sa(j)+f(i)*cos((j)*2*pi*(i-1-N/2)/N));
+                Sb(j) = (Sb(j)+f(i)*sin((j)*2*pi*(i-1-N/2)/N));        
+            end
+
+        end
+        for j=1:K
+            Sa(j)=Sa(j)*(1/(N/2));
+            Sb(j)=Sb(j)*(1/(N/2));
+           % Saa(j)= 4*(-1)^j/(j^2);%теоретически определенный коэф. аk для функции t^2
+        end
+        SSa=Sa; %коэффициенты ak
+        SSb=Sb; %коэффициенты bk4fi_res=atan(SSa(kp)/SSb(kp))
+        %SSaa=Saa %теоретически определенные коэф. аk для функции t^2 
+        % i=1:K;
+        % figure 
+        % plot(i,Sa);
+        % title('Коэффициенты Sa');
+        %Вычисление и отображение спектра амплитуд (начало)
+        for j=1:K 
+        Sab(j)=sqrt(Sa(j)^2+Sb(j)^2);
+        end
+        i=1:K;
+        %figure 
+        %plot(i,Sab);
+        %ТУТ
+        %stem(Sab(1:K)); %вывод графика  дискретной последовательности данных
+        %axis([1 8 -0.2 1.2]);%задание осей: [xmin xmax ymin ymax]
+        %title('Амплитуды частотных составляющих спектра');
+        %xlabel('Количество периодов')
+        %axis tight;
+        %Вычисление и отображение спектра амплитуд (конец)
+        y=zeros(1,N+1);
+        for i=1:N+1
+            for j=1:K
+                y(i)= y(i)+Sa(j)*cos(j*2*pi*(i-1-N/2)/N)+Sb(j)*sin(j*2*pi*(i-1-N/2)/N);        
+            end  
+             y(i)=Sa0+y(i);
+        end
+        i=1:N+1;
+        %ТУТ
+        %figure
+        %plot(i,f);
+        %axis tight;
+        %hold on;
+        %plot(i,y,'r-')
+        %hold off;
+
+        for i=1:N+1
+          dy(i)=y(i)-f(i);%абсолютная погрешность восстановления
+        end
+        dy_proc=dy/max(f)*100;
+        CKO=std(dy);
+        
+        %fprintf('N=%i K=%i\n', N,K);
+        fprintf('%f\n', std(dy_proc));
+        %CKO_proc=std(dy_proc)%СКО в процентах
+        Kpow=Kpow+1;
+    end
+    fprintf('End: N=%i K=%i\n', N,K);
+    Npow=Npow+1;
+end
+
+currentCharacterEncoding = slCharacterEncoding();
+fprintf('%s', currentCharacterEncoding);
+
+pause;
+close all;
+clear;
